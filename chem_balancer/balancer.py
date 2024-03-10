f9f64699f5a296b1ee2092f7ea64e9f42114dad0
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-10 18:04:11 +0330

    :hammer: chore: implement the parse_equation method

--- chem_balancer/balancer.py
+++ chem_balancer/balancer.py
@@ -1,2 +1,18 @@
+import numpy as np
+
+
 class ChemicalEquationBalancer:
-    pass
+    def __init__(self, elements: list, formula: str):
+        self.elements = elements
+        self.formula = formula
+        self.matrix = np.ndarray([])
+
+    def parse_equation(self) -> (str, str):
+        row_num = len(self.elements)
+        l_formula = self.formula.split('->')[0].strip()
+        r_formula = self.formula.split('->')[1].strip()
+        col_num = len(self.formula.split('+')) + len(self.formula.split('->')) - 1
+        self.matrix = np.zeros((row_num, col_num + 1))
+        return l_formula, r_formula
+
+   
