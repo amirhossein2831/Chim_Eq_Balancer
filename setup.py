b13a8c456ee1c9ac232c036ab5a86af93051814c
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-10 20:11:17 +0330

    :heavy_plus_sign: feat: add setup.py to make it useable package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chemical_eq_balancer',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['numpy'],
+    author='AmirHossein',
+    author_email='amirmemool12@gmail.com',
+    description='a package to balance simple chemical equations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/amirhossein2831/Chim_Eq_Balancer',
+    license='MIT',
+)
