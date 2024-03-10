e408b389c94e4ab7e9ed2e61212c9268cdf321e9
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-10 19:41:38 +0330

    :hammer: chore: update some logic

--- chem_balancer/balancer.py
+++ chem_balancer/balancer.py
@@ -5,20 +5,19 @@ class ChemicalEquationBalancer:
     def __init__(self, elements: list, formula: str):
         self.elements = elements
         self.formula = formula
+        self.l_formula = self.formula.split('->')[0].strip()
+        self.r_formula = self.formula.split('->')[1].strip()
         self.matrix = np.ndarray([])
 
-    def parse_equation(self) -> (str, str):
+    def parse_equation(self) -> None:
         row_num = len(self.elements)
-        l_formula = self.formula.split('->')[0].strip()
-        r_formula = self.formula.split('->')[1].strip()
         col_num = len(self.formula.split('+')) + len(self.formula.split('->')) - 1
         self.matrix = np.zeros((row_num, col_num + 1))
-        return l_formula, r_formula
 
     def fill_matrix(self) -> None:
-        l_formula, r_formula = self.parse_equation()
+        self.parse_equation()
         col = 0
-        for i in l_formula.split('+'):
+        for i in self.l_formula.split('+'):
             for j in i.split():
                 index = self.elements.index(j[0])
                 if len(j) == 1:
@@ -28,7 +27,7 @@ class ChemicalEquationBalancer:
 
             col += 1
 
-        for i in r_formula.split('+'):
+        for i in self.r_formula.split('+'):
             for j in i.split():
                 index = self.elements.index(j[0])
                 if len(j) == 1:
